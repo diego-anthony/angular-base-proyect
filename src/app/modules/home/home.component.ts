263c02aca343380c0cc19cef2cafa465233403c3
Diego Salazar <externo.dsalazar@essalud.gob.pe>
2020-08-27 10:03:29 -0500

    chore: remove unused imports

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,16 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { RoutePath } from '@core/configs/route-path.config';
-import { RouteUtil } from '@core/utils/route.util';
-
-interface marker {
-  lat: number;
-  lng: number;
-  label?: string;
-  draggable: boolean;
-}
-
-declare var ol: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
