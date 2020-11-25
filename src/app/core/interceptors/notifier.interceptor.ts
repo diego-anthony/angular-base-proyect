d0df9320baccf9ab08466113a03a8e56ca9e2f1e
Diego Salazar <externo.dsalazar@essalud.gob.pe>
2020-11-25 10:32:13 -0500

    feat: font concord

--- src/app/core/interceptors/notifier.interceptor.ts
+++ src/app/core/interceptors/notifier.interceptor.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpEvent,
-    HttpInterceptor,
-    HttpHandler,
-    HttpRequest,
-    HttpErrorResponse
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpErrorResponse,
 } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
@@ -14,31 +14,36 @@ import HttpStatusCode from '@core/enums/http-status-code.enum';
 
 @Injectable({ providedIn: 'root' })
 export class NotifierInterceptor implements HttpInterceptor {
-    constructor(
-        private _notification: NotificationService
-    ) { }
+  constructor(private _notification: NotificationService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request)
-            .pipe(
-                catchError((response: HttpErrorResponse) => {
-                    switch (response.status) {
-                        case 0:
-                            this._notification.error('No se pudo acceder al servidor. Virifique su conexión a internet.');
-                            break;
-                        case HttpStatusCode.BAD_REQUEST:
-                            this._notification.warning(response.error.message);
-                            break;
-                        case HttpStatusCode.INTERNAL_SERVER_ERROR:
-                            this._notification.error('Error al procesar información.');
-                            break;
-                        case HttpStatusCode.NOT_FOUND:
-                            // TODO: Redireccionar a página 404
-                            this._notification.error('Recurso no encontrado.');
-                            break;
-                    }
-                    return throwError(response);
-                })
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((response: HttpErrorResponse) => {
+        switch (response.status) {
+          case 0:
+            this._notification.error(
+              'No se pudo acceder al servidor. Verifique su conexión a internet.'
             );
-    }
+            break;
+          case HttpStatusCode.BAD_REQUEST:
+            const message = response?.error?.message;
+            if (message) {
+              this._notification.warning(message);
+            }
+            break;
+          case HttpStatusCode.INTERNAL_SERVER_ERROR:
+            this._notification.error('Error al procesar información.');
+            break;
+          case HttpStatusCode.NOT_FOUND:
+            // TODO: Redireccionar a página 404
+            this._notification.error('Recurso no encontrado.');
+            break;
+        }
+        return throwError(response);
+      })
+    );
+  }
 }
