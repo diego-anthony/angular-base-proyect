4409a4ea1dffb4f0c939792378249f1817b59be1
Diego Salazar <externo.dsalazar@essalud.gob.pe>
2020-11-25 11:15:21 -0500

    chore: spinner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,20 @@ import { Component } from '@angular/core';
 import { SeoService } from '@core/services/common/seo.service';
 
 @Component({
-    selector: 'app-root',
-    template: `
-    <app-environment-indicator></app-environment-indicator>
+  selector: 'app-root',
+  template: `
     <router-outlet></router-outlet>
-    `,
+    <ngx-spinner
+      bdColor="rgba(0, 0, 0, 0.8)"
+      size="medium"
+      color="#fff"
+      type="ball-clip-rotate"
+      [fullScreen]="true"
+      ><p style="color: white">Cargando...</p></ngx-spinner
+    >
+    <app-environment-indicator></app-environment-indicator>
+  `,
 })
 export class AppComponent {
-    constructor(private _seoService: SeoService) {}
+  constructor(private _seoService: SeoService) {}
 }
